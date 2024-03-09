90a3492b7e54045c9a8125ec796e7e926ba39374
Ahmed Mahmoud <Ahmed-abdulhamid@hotmail.com>
2024-03-09 12:53:44 +0300

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.dtFrom = New System.Windows.Forms.DateTimePicker()
+        Me.dtTo = New System.Windows.Forms.DateTimePicker()
+        Me.dt1 = New System.Windows.Forms.DateTimePicker()
+        Me.dt2 = New System.Windows.Forms.DateTimePicker()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(382, 102)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'dtFrom
+        '
+        Me.dtFrom.CustomFormat = "dd/MM/yyyy"
+        Me.dtFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtFrom.Location = New System.Drawing.Point(257, 25)
+        Me.dtFrom.Name = "dtFrom"
+        Me.dtFrom.Size = New System.Drawing.Size(200, 20)
+        Me.dtFrom.TabIndex = 1
+        '
+        'dtTo
+        '
+        Me.dtTo.CustomFormat = "dd/MM/yyyy"
+        Me.dtTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dtTo.Location = New System.Drawing.Point(42, 25)
+        Me.dtTo.Name = "dtTo"
+        Me.dtTo.Size = New System.Drawing.Size(200, 20)
+        Me.dtTo.TabIndex = 1
+        '
+        'dt1
+        '
+        Me.dt1.CustomFormat = "dd/MM/yyyy"
+        Me.dt1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dt1.Location = New System.Drawing.Point(257, 66)
+        Me.dt1.Name = "dt1"
+        Me.dt1.Size = New System.Drawing.Size(200, 20)
+        Me.dt1.TabIndex = 1
+        '
+        'dt2
+        '
+        Me.dt2.CustomFormat = "dd/MM/yyyy"
+        Me.dt2.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.dt2.Location = New System.Drawing.Point(42, 66)
+        Me.dt2.Name = "dt2"
+        Me.dt2.Size = New System.Drawing.Size(200, 20)
+        Me.dt2.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(469, 298)
+        Me.Controls.Add(Me.dt2)
+        Me.Controls.Add(Me.dt1)
+        Me.Controls.Add(Me.dtTo)
+        Me.Controls.Add(Me.dtFrom)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents dtFrom As System.Windows.Forms.DateTimePicker
+    Friend WithEvents dtTo As System.Windows.Forms.DateTimePicker
+    Friend WithEvents dt1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents dt2 As System.Windows.Forms.DateTimePicker
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Public Structure SDateInterval
+        Dim StartDate As Date    ' تاريخ بداية الفترة
+        Dim EndDate As Date     ' تاريخ نهاية الفترة
+    End Structure
+
+    ' دالة الاختبار
+    Public Function IntervalConflict(ByVal Period1 As SDateInterval, ByVal Period2 As SDateInterval) As Boolean
+        If ((Period1.StartDate >= Period2.StartDate) And (Period1.StartDate <= Period2.EndDate)) Or _
+                ((Period1.EndDate >= Period2.StartDate) And (Period1.EndDate <= Period2.EndDate)) Then
+            Return True    ' تعارض
+        Else
+            Return False    ' لا يوجد تعارض
+        End If
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Period1 As SDateInterval
+        Dim Period2 As SDateInterval
+
+        ' القيم التالية للتجربة - يمكن قراءتها من مربعات نص أو غيرها
+        Period1.StartDate = dtFrom.Value.ToShortDateString
+        Period1.EndDate = dtTo.Value.ToShortDateString
+
+        Period2.StartDate = dt1.Value.ToShortDateString
+        Period2.EndDate = dt2.Value.ToShortDateString
+
+        If IntervalConflict(Period2, Period1) Then
+            MsgBox("Conflicts")
+        Else
+            MsgBox("No conflict")
+        End If
+    End Sub
+End Class
